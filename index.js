a91f9eb97560526216b823a08296a0454a5300f3
Abdulraqeeb-Mohammed <a.raqeeb@comptechco.com>
2023-06-01 12:12:42 +0300

    ...

---
+++ index.js
@@ -0,0 +1,17 @@
+const puppeteer = require ('puppeteer');
+
+async function run(){
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+
+    await page.goto('https://www.traversymedia.com');
+
+    // await page.screenshot({path: 'example.png' , fullPage : true });
+    await page.pdf({path: 'example.pdf' , format: 'A4' });
+
+    await browser.close;
+}
+
+
+run();
